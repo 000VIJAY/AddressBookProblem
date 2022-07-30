341d27826c32bba3098071dfa6d434182601f77d
000VIJAY <vijay893689@gmail.com>
2022-07-30 20:04:08 +0530

    vijay added program for add contact in addressbook

--- AddressBook/PersonAddress.cs
+++ AddressBook/PersonAddress.cs
@@ -10,13 +10,44 @@ namespace AddressBook
     {
         public List<Contact> persons = new List<Contact>();
 
-        public void Book(Contact con)
+        public void Book()
         {
 
             persons.Add(new Contact() { FirstName = "Vijay", LastName = "kumar", City = "Patna", State = "Bihar", Zip = 8000001, PhoneNumber = 87654321287, Email = "vijya87348@gmail.com", });
             persons.Add(new Contact() { FirstName = "Nitish", LastName = "kumar", City = "Patna", State = "Bihar", Zip = 800005, PhoneNumber = 654321287, Email = "vijya87348@gmail.com", });
             persons.Add(new Contact() { FirstName = "Rahul ", LastName = "Yadav", City = "Delhi", State = "New Delhi", Zip = 534342, PhoneNumber = 43523254234, Email = "rahul@gmail.com" });
             persons.Add(new Contact() { FirstName = "Rohal ", LastName = "Goyal", City = "Delhi", State = "New Delhi", Zip = 534342, PhoneNumber = 76523254234, Email = "rohan@gmail.com" });
+            Display();
+        }
+        public void Show()
+        {
+            Console.WriteLine("Enter Firstname");
+            string? FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string? LastName = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string? City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string? State = Console.ReadLine();
+            Console.WriteLine("Enter zip Number : only digit is valid");
+            int Zip = (int)Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            long PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your email");
+            string? Email = Console.ReadLine();
+            persons.Add(new Contact()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                City = City,
+                State = State,
+                Zip = Zip,
+                PhoneNumber = PhoneNumber,
+                Email = Email,
+            });
+        }
+        public void Display ()
+        {
             foreach (var Contact in persons)
             {
                 Console.WriteLine("Your name is :{0} ", Contact.FirstName + " " + Contact.LastName);
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,21 @@
 ﻿using AddressBook;
-Contact person = new Contact();
 PersonAddress GetPerson = new PersonAddress();
-GetPerson.Book(person);
+bool check = true;
+
+while (check)
+{
+        Console.WriteLine("Enter -0 ,For stop execution");
+        Console.WriteLine("Enter-1,For display the information /n");
+    int num = Convert.ToInt32(Console.ReadLine());
+    switch (num)
+    {
+        case 0:
+          check = false;
+            break;
+        case 1:
+            GetPerson.Show();
+            GetPerson.Book();
+            break;
+    }
+}
+
