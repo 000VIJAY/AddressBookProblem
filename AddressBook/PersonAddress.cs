7d070af72d13971c6c0b517da1de3e2a54a57c1b
000VIJAY <vijay893689@gmail.com>
2022-07-30 20:17:43 +0530

    Vijay added program for edit contact

--- AddressBook/PersonAddress.cs
+++ AddressBook/PersonAddress.cs
@@ -19,7 +19,7 @@ namespace AddressBook
             persons.Add(new Contact() { FirstName = "Rohal ", LastName = "Goyal", City = "Delhi", State = "New Delhi", Zip = 534342, PhoneNumber = 76523254234, Email = "rohan@gmail.com" });
             Display();
         }
-        public void Show()
+        public void Addcontact()
         {
             Console.WriteLine("Enter Firstname");
             string? FirstName = Console.ReadLine();
@@ -46,8 +46,69 @@ namespace AddressBook
                 Email = Email,
             });
         }
-        public void Display ()
+        public void EditContact()
         {
+            //only edit the added contact
+            Console.WriteLine("To edit contact list enter First Name");
+            string name = Console.ReadLine().ToLower();
+
+            foreach (var Contacts in persons)
+            {
+                if (persons.Contains(Contacts))
+                {
+                    if (Contacts.FirstName.Equals(name))
+                    {
+                        Console.WriteLine("enter the information wants to edit");
+                        Console.WriteLine("For edit First Name enter - 0");
+                        Console.WriteLine("For edit Last Name enter - 1");
+                        Console.WriteLine("For edit City  enter - 2");
+                        Console.WriteLine("For edit State  enter - 3");
+                        Console.WriteLine("For edit Zip  enter - 4");
+                        Console.WriteLine("For edit Phone  enter - 5");
+                        Console.WriteLine("For edit Email  enter - 6");
+                        int edit = Convert.ToInt32(Console.ReadLine());
+                        switch (edit)
+                        {
+                            case 0:
+                                Console.WriteLine("Enter the First Name");
+                                Contacts.FirstName = Console.ReadLine();
+                                break;
+                            case 1:
+                                Console.WriteLine("Enter the Last Name");
+                                Contacts.LastName = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter city Name");
+                                Contacts.City = Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter State Name");
+                                Contacts.State = Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter Zip ");
+                                Contacts.Zip = Convert.ToDouble(Console.ReadLine());
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter Phone Number");
+                                Contacts.PhoneNumber = Convert.ToDouble(Console.ReadLine());
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter Email");
+                                Contacts.Email = Console.ReadLine();
+                                break;
+                            default:
+                                Console.WriteLine("Contact not exixt");
+                                break;
+                        }
+                    }
+
+                }
+            }
+                Display();
+        }
+            public void Display ()
+            {
             foreach (var Contact in persons)
             {
                 Console.WriteLine("Your name is :{0} ", Contact.FirstName + " " + Contact.LastName);
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,7 +5,8 @@ bool check = true;
 while (check)
 {
         Console.WriteLine("Enter -0 ,For stop execution");
-        Console.WriteLine("Enter-1,For display the information /n");
+        Console.WriteLine("Enter-1,For Add the information /n");
+        Console.WriteLine("Enter -2,For Edit information");
     int num = Convert.ToInt32(Console.ReadLine());
     switch (num)
     {
@@ -13,9 +14,13 @@ while (check)
           check = false;
             break;
         case 1:
-            GetPerson.Show();
+            GetPerson.Addcontact();
             GetPerson.Book();
             break;
+        case 2:
+            string name = Console.ReadLine();
+            GetPerson.EditContact()
+        ; break;
     }
 }
 
